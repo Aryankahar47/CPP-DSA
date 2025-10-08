fd9002650cab519db4e5139390f4b9e546c1ce4e
Aryan Kahar <aryankahar463@gmail.com>
2025-10-08 23:39:49 +0530

    OOP concepts, constructor

---
+++ oop/index.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Student{
+    
+      string name;
+      float cgpa;
+      public:
+      Student(){
+        cout<<"costructor without parameter is called"<<endl;
+      }
+      Student(string name){
+        cout<<"constructor with parameter is called"<<endl;
+        this->name =name;
+        cout<<name<<endl;
+      }
+      void getPercentage(){
+        cout<<(cgpa*10);
+      }
+
+      void setName(string nameVal){
+        name = nameVal;
+      }
+
+      string getName(){
+        return name;
+      }
+};
+
+class profile{
+    string email;
+    string username;
+    string password;
+    string bio;
+    void deactivate(){
+        cout<<"deactivate account";
+    }
+    void editBio(string newBio){
+         bio = newBio;
+    }
+};
+
+int main(){
+    Student s1("Babu");
+    Student s2("Kahar");
+    s1.setName("Aryan");
+    cout<<s1.getName()<<endl;
+    Student s3("helllo");
+    cout<<s3.getName()<<endl;
+    Student s4;
+    
+    return 0;
+}
