c7fb8487419495488379fd9d617379fd48b5b577
Aryan Kahar <aryankahar463@gmail.com>
2026-01-15 04:21:41 +0530

    binary tree, build, inorder, preorder

---
+++ binaryTree/inorderTraversal/index.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+class Node{
+    public:
+    int data;
+    Node* left, *right;
+    Node(int data){
+        this->data=data;
+        left=right=NULL;
+    }
+};
+
+static int i=-1;
+Node* buildTree(vector<int> nodes){
+    i++;
+    if(nodes[i]==-1){
+        return NULL;
+    }
+
+    Node* currNode = new Node(nodes[i]);
+    currNode->left=buildTree(nodes);
+    currNode->right=buildTree(nodes);
+    return currNode;
+}
+
+void inorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+int main(){
+vector<int> nodes={1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+      Node* root = buildTree(nodes);
+      inorder(root);
+      return 0;
+}
